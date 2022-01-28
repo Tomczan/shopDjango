91b4101f98fb99753aecb2d17848a26a04de6350
Tomasz Czuba <t.czuba95@gmail.com>
2022-01-28 22:49:10 +0100

    add: possibility to change iteam quantity in cart
    moved dunder methots in cart to the top

--- cart/cart.py
+++ cart/cart.py
@@ -16,33 +16,6 @@ class Cart(object):
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
 
-    def add(self, product, quantity=1, update_quantity=False):
-        """
-        Dodanie produktu do koszyka lub zmiana jego ilosci
-        """
-        product_id = str(product.id)
-        if product_id not in self.cart:
-            self.cart[product_id] = {
-                'quantity': 0, 'price': str(product.price)}
-        if update_quantity:
-            self.cart[product_id]['quantity'] = quantity
-        else:
-            self.cart[product_id]['quantity'] += quantity
-        self.save()
-
-    def save(self):
-        # oznaczenie sesji jako zmodyfikowanej, by upewnic sie o jej zapisaniu
-        self.session.modified = True
-
-    def remove(self, product):
-        """
-        Usuniecie produktu z koszyka
-        """
-        product_id = str(product.id)
-        if product_id in self.cart:
-            del self.cart[product_id]
-            self.save()
-
     def __iter__(self):
         """
         Iteracja przez elementy koszyka na zakupy i pobranie produktow z bazy danych
@@ -63,6 +36,34 @@ class Cart(object):
         Liczba wszystkich elementów w koszyku.
         """
         return sum(item['quantity'] for item in self.cart.values())
+
+    def add(self, product, quantity=1, update_quantity=False):
+        """
+        Dodanie produktu do koszyka lub zmiana jego ilosci
+        """
+        product_id = str(product.id)
+        if product_id not in self.cart:
+            self.cart[product_id] = {
+                'quantity': 0, 'price': str(product.price)}
+        '''if update_quantity:
+            self.cart[product_id]['quantity'] = quantity
+        else:
+            self.cart[product_id]['quantity'] = quantity'''
+        self.cart[product_id]['quantity'] = quantity
+        self.save()
+
+    def save(self):
+        # oznaczenie sesji jako zmodyfikowanej, by upewnic sie o jej zapisaniu
+        self.session.modified = True
+
+    def remove(self, product):
+        """
+        Usuniecie produktu z koszyka
+        """
+        product_id = str(product.id)
+        if product_id in self.cart:
+            del self.cart[product_id]
+            self.save()
 
     def get_total_price(self):
         """
