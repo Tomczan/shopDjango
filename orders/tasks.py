b999dc31a9fa05588bb7cd1323f2093bb9015601
Tomasz Czuba <t.czuba95@gmail.com>
2022-02-02 18:35:52 +0100

    fixes

--- orders/tasks.py
+++ orders/tasks.py
@@ -10,9 +10,6 @@ def order_created(order_id):
     """
     order = Order.objects.get(id=order_id)
     subject = f'Zamówienie nr {order_id}'
-    message = f'Witaj, {order.first_name}, \nZłożyłes zamówienie w naszym sklepie.\
-                 Identyfikator zamówienia to{order.id}'
-
+    message = f'Witaj, {order.first_name}, \nZłożyłes zamówienie w naszym sklepie. Id zamówienia to {order.id}'
     mail_sent = send_mail(subject, message, 'admin@shop.com', [order.email])
-
     return mail_sent
