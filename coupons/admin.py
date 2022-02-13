24ea0e27b0394321a0bab080f285902155648922
Tomasz Czuba <t.czuba95@gmail.com>
2022-02-13 17:36:10 +0100

    registered Coupon model in Admin panel

--- coupons/admin.py
+++ coupons/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+class CouponAdmin(admin.ModelAdmin):
+    list_display = ['code', 'valid_from', 'valid_to', 'discount', 'active']
+    list_filter = ['active', 'valid_from', 'valid_to']
+    search_fields = ['code']
